1dedae1320aa3cec9f06d08ac5c2cb0f7c111030
Berk Orbay <berkorbay@gmail.com>
2016-12-16 18:02:09 +0300

    documentation v2

--- R/veri_setleri.r
+++ R/veri_setleri.r
@@ -11,6 +11,27 @@
 #'@param kayitli_secmen İlgili sandıkta (eğer `cevre_turu` Toplam ise ilgili bölgede) kayıtlı toplam seçmen sayısını verir.
 #'@param oy_kullanan İlgili sandıkta (eğer `cevre_turu` Toplam ise ilgili bölgede) oy kullanan toplam kişi sayısını verir. Bazı sandıklarda oy kullanan kişi sayısı kayıtlı seçmen sayısından fazla olabilir. Bunun sebepleri arasında görevlilerin (ör. sandık görevlisi, polis memuru) tutanakla kendi kayıtlı sandıklarının yerine başka sandıklarda oy atabilmeleri ve kayıt hataları bulunmaktadır.
 #'@param gecerli_oy İlgili sandıktaki (eğer `cevre_turu` Toplam ise ilgili bölgede) geçerli oy sayısını verir. Oylar çeşitli sebeplerle geçerli sayılmayabilmektedir. (Ör. Boş oy, çift mühür, oyun nereye basıldığının anlaşılamaması.)
+#'@param ak_parti Adalet ve Kalkınma Partisi (AK PARTİ) 14 Ağustos 2001 tarihinde kurulmuştur. Merkez sağ partisidir. İdeolojileri sosyal muhafazakarlık, muhafazakar demokrasi, ekonomik liberalizm ve yeni Osmanlıcılıktır. 2002 genel seçimlerinden Kasım 2015 genel seçimlerine kadar en yüksek oyu alıp iktidar partisi olmuşlardır. (Kaynak: Vikipedi)
+#'@param chp Cumhuriyet Halk Partisi (CHP) 9 Eylül 1923'te Mustafa Kemal Atatürk tarafından kurulmuştur. Merkez sol partisidir. İdeolojileri Atatürkçülük, sosyal demokrasi ve demokratik soldur. 2002 genel seçimlerinden Kasım 2015 genel seçimlerine kadar en yüksek ikinci oyu alıp ana muhalefet partisi olmuşlardır. (Kaynak: Vikipedi)
+#'@param mhp Milliyetçi Hareket Partisi (MHP) 9 Şubat 1969 tarihinde kurulmuştur. Sağ partidir. İdeolojileri Türk milliyetçiliği, ülkücülük ve dokuz ışıktır. Kasım 2015 genel seçimlerinde en yüksek üçüncü oyu alıp muhalefet olmuşlardır. (Kaynak: Vikipedi)
+#'@param hdp Halkların Demokratik Partisi (HDP) 27 Ekim 2013 tarihinde kurulmuştur. Sol partidir. İdeolojileri siyasi çoğulculuk, sosyalizm, radikal demokrasi, demokratik sosyalizm, feminizm ve doğrudan demokrasidir. Kasım 2015 genel seçimlerinde en yüksek dördüncü oyu alıp muhalefet olmuşlardır. (Kaynak: Vikipedi)
+#'@param dyp Doğru Yol Partisi (DYP)
+#'@param anadolu_partisi Anadolu Partisi
+#'@param hak_par Hak ve Özgürlükler Partisi (HAK-PAR)
+#'@param kp Komünist Parti (KP)
+#'@param millet_partisi Millet Partisi
+#'@param hap Hak ve Adalet Partisi (HAP)
+#'@param mep Merkez Parti (MEP)
+#'@param turk_parti Toplumsal Uzlaşma Reform ve Kalkınma Partisi (TURK Parti)
+#'@param hkp Halkın Kurtuluş Partisi (HKP)
+#'@param ldp Liberal Demokrat Parti (LDP)
+#'@param saadet_partisi Saadet Partisi
+#'@param dsp Demokratik Sol Parti (DSP)
+#'@param yurt_parti Yurt Partisi
+#'@param dp Demokrat Parti (DP)
+#'@param vatan_partisi Vatan Partisi
+#'@param btp Bağımsız Türkiye Partisi (BTP)
+#'@param bagimsiz Bağımsız adaylar.
 "secim150607g"
 
 #'@title 1 Kasım 2015 Genel Seçimleri Sandık Bazında Seçmen ve Oy Sayıları
@@ -26,4 +47,20 @@
 #'@param kayitli_secmen İlgili sandıkta (eğer `cevre_turu` Toplam ise ilgili bölgede) kayıtlı toplam seçmen sayısını verir.
 #'@param oy_kullanan İlgili sandıkta (eğer `cevre_turu` Toplam ise ilgili bölgede) oy kullanan toplam kişi sayısını verir. Bazı sandıklarda oy kullanan kişi sayısı kayıtlı seçmen sayısından fazla olabilir. Bunun sebepleri arasında görevlilerin (ör. sandık görevlisi, polis memuru) tutanakla kendi kayıtlı sandıklarının yerine başka sandıklarda oy atabilmeleri ve kayıt hataları bulunmaktadır.
 #'@param gecerli_oy İlgili sandıktaki (eğer `cevre_turu` Toplam ise ilgili bölgede) geçerli oy sayısını verir. Oylar çeşitli sebeplerle geçerli sayılmayabilmektedir. (Ör. Boş oy, çift mühür, oyun nereye basıldığının anlaşılamaması.)
+#'@param ak_parti Adalet ve Kalkınma Partisi (AK PARTİ) 14 Ağustos 2001 tarihinde kurulmuştur. Merkez sağ partisidir. İdeolojileri sosyal muhafazakarlık, muhafazakar demokrasi, ekonomik liberalizm ve yeni Osmanlıcılıktır. 2002 genel seçimlerinden Kasım 2015 genel seçimlerine kadar en yüksek oyu alıp iktidar partisi olmuşlardır. (Kaynak: Vikipedi)
+#'@param chp Cumhuriyet Halk Partisi (CHP) 9 Eylül 1923'te Mustafa Kemal Atatürk tarafından kurulmuştur. Merkez sol partisidir. İdeolojileri Atatürkçülük, sosyal demokrasi ve demokratik soldur. 2002 genel seçimlerinden Kasım 2015 genel seçimlerine kadar en yüksek ikinci oyu alıp ana muhalefet partisi olmuşlardır. (Kaynak: Vikipedi)
+#'@param mhp Milliyetçi Hareket Partisi (MHP) 9 Şubat 1969 tarihinde kurulmuştur. Sağ partidir. İdeolojileri Türk milliyetçiliği, ülkücülük ve dokuz ışıktır. Kasım 2015 genel seçimlerinde en yüksek üçüncü oyu alıp muhalefet olmuşlardır. (Kaynak: Vikipedi)
+#'@param hdp Halkların Demokratik Partisi (HDP) 27 Ekim 2013 tarihinde kurulmuştur. Sol partidir. İdeolojileri siyasi çoğulculuk, sosyalizm, radikal demokrasi, demokratik sosyalizm, feminizm ve doğrudan demokrasidir. Kasım 2015 genel seçimlerinde en yüksek dördüncü oyu alıp muhalefet olmuşlardır. (Kaynak: Vikipedi)
+#'@param bbp Büyük Birlik Partisi (BBP)
+#'@param btp Bağımsız Türkiye Partisi (BTP)
+#'@param dp Demokrat Parti (DP)
+#'@param dsp Demokratik Sol Parti (DSP)
+#'@param dyp Doğru Yol Partisi (DYP)
+#'@param hak_par Hak ve Özgürlükler Partisi (HAK-PAR)
+#'@param hkp Halkın Kurtuluş Partisi (HKP)
+#'@param ldp Liberal Demokrat Parti (LDP)
+#'@param millet_partisi Millet Partisi
+#'@param saadet_partisi Saadet Partisi
+#'@param vatan_partisi Vatan Partisi
+#'@param bagimsiz Bağımsız adaylar.
 "secim151101g"
